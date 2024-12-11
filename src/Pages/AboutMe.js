816cfdb76fbcdde868f7ceadf6fc2914b28b7e7a
heidispalitta <heidispalitta@gmail.com>
2024-12-10 19:06:40 -0600

    Update AboutMe.js

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -6,14 +6,12 @@ const AboutMe = () => {
             <h1>About Me</h1>
             <h2>Deluxe Edition</h2>
             <body>
-                I've lived in the Madison Area in Wisconsin for about 10 years and before that lived in San Jose, California.
-                Half of my family is in Romania so I spent many summers there while growing up (I'm also fluent in Romanian).
+                I've lived in the Madison area in Wisconsin for about 10 years and before that lived in San Jose, California.
+                Half of my family is in Romania so I spent many summers there growing up (I'm also fluent in Romanian). 
             </body>
             <body>
-                From elementary school to high school I was conviced that I was going to go to art school and become an artist full-time.
-                While that didn't end up being the case I do still like making art when I can find the time. 
-                Actually, I'd also say I use art as a skill a lot more in my work than I thought I would when I decided to go into Computer Science.
-                Obviously art is important when thinking about inteface design but even more than that I've found myself having to make a number of animations and graphics for app development as a part of my work in the People and Robots Lab.
+                I enjoy making visual art in my free time (oil painting and sculpture primarily, but I've dabbled in a bunch of mediums).
+                I also enjoy cooking and collecting antique textbooks. My best find is/was a German anatomy textbook from 1855.
             </body>
         </section>
     );
