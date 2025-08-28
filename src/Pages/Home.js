5a1497d192812704ac975f42b25a9fdea2b413e3
heidispalitta <heidispalitta@gmail.com>
2025-08-27 21:06:55 -0500

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,11 +4,8 @@ const Home = () => {
     return (
         <section id="Home" className="content-section">
             <h1>Heidi Spalitta</h1>
-            <h2>I am a student at the <strong>University of Wisconsin-Madison</strong> completing a B.S. in Computer Science and Anthropology with an emphasis on Human-Computer Interaction. </h2>
-            <h2>I have been working in the <strong>People and Robots Lab</strong> since February 2023 as an Undergraduate Research Assistant. 
-                As a part of my work within the Assistive Technology for Adults with Down Syndrome project I have helped with iOS development, data analysis, and interface design.
-                I am additionally working on a <strong>thesis project</strong> this year through the People and Robots Lab. 
-            </h2>
+            <h2>I am a first year Computer Science PhD student in the <strong>People and Robots Lab</strong> at the <strong>University of Wisconsin-Madison</strong>.</h2>
+            <h2>I earned my B.S. in Computer Science and Anthropology at UW-Madison in Spring of 2025.</h2>
         </section>
     );
 }
