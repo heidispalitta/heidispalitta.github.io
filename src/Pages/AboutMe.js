5de92802ef3045c15eb61cc310495e8548e296e3
heidispalitta <heidispalitta@gmail.com>
2026-02-04 20:57:22 -0600

    abme

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -6,12 +6,8 @@ const AboutMe = () => {
             <h1>About Me</h1>
             <h2>Deluxe Edition</h2>
             <body>
-                I've lived in the Madison area in Wisconsin for about 10 years and before that lived in San Jose, California.
-                Half of my family is in Romania so I spent many summers there growing up (I'm also fluent in Romanian). 
-            </body>
-            <body>
-                I enjoy making visual art in my free time (oil painting and sculpture primarily, but I've dabbled in a bunch of mediums).
-                I also enjoy cooking and collecting antique textbooks. My best find is a German anatomy textbook from 1855.
+                In addition to research I enjoy painting when I can find the time (oil painting is my preference, but I've dabbled in a bunch of mediums).
+                I also enjoy cooking, open-world RPGs, and collecting antique textbooks. 
             </body>
         </section>
     );
