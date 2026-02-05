5fb857a884bc82a0628f1883b7aa9b4e4072f330
heidispalitta <heidispalitta@gmail.com>
2026-02-04 20:09:11 -0600

    new alignment

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,8 +4,10 @@ const Home = () => {
     return (
         <section id="Home" className="content-section">
             <h1>Heidi Spalitta</h1>
-            <h2>I am a first year Computer Science PhD student in the <strong>People and Robots Lab</strong> at the <strong>University of Wisconsin-Madison</strong>.</h2>
-            <h2>I earned my B.S. in Computer Science and Anthropology at UW-Madison in Spring of 2025.</h2>
+            <section className="content-subsection">
+                <h2>I am a first year Computer Science PhD student in the <strong>People and Robots Lab</strong> at the <strong>University of Wisconsin-Madison</strong>.</h2>
+                <h2>I earned my B.S. in Computer Science and Anthropology at UW-Madison in Spring of 2025.</h2>
+            </section>
         </section>
     );
 }
